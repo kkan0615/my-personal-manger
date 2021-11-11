87018ee50a5b40d33933bfc3761133f9cd7a7273
kkan0615 <kkan0615@gmail.com>
2021-11-11 23:03:11 +0900

    :rocket: version 0.0.4c

--- src/main/services/manager.js
+++ src/main/services/manager.js
@@ -6,22 +6,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.createManager = void 0;
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-const Manager_1 = require("../types/models/Manager");
+const electron_is_dev_1 = __importDefault(require("electron-is-dev"));
+const uuid_1 = require("uuid");
 const createManager = (payload) => {
-    const dataFolder = path_1.default.join(__dirname, '..', 'data');
+    const dataFolder = electron_is_dev_1.default ? path_1.default.join(__dirname, '..', 'data') : path_1.default.join(process.resourcesPath, 'data');
     /* Data data folder is not exist */
     if (!fs_1.default.existsSync(dataFolder)) {
         fs_1.default.mkdirSync(dataFolder);
     }
-    const fileList = fs_1.default.readdirSync(dataFolder).filter(file => file.includes(Manager_1.MANAGER_FILE_PREFIX));
+    // const fileList = fs.readdirSync(dataFolder).filter(file => file.includes(MANAGER_FILE_PREFIX))
     /* First slot */
-    if (!fileList.length) {
-        fs_1.default.writeFileSync(`${dataFolder}/${Manager_1.MANAGER_FILE_PREFIX}1`, JSON.stringify(payload));
-    }
-    else {
-        const num = Number(fileList[fileList.length - 1].split(Manager_1.MANAGER_FILE_PREFIX)[1]);
-        fs_1.default.writeFileSync(`${dataFolder}/${Manager_1.MANAGER_FILE_PREFIX}${num + 1}`, JSON.stringify(payload));
-    }
+    // if (!fileList.length) {
+    const name = (0, uuid_1.v4)();
+    fs_1.default.mkdirSync(`${dataFolder}/${name}`);
+    fs_1.default.writeFileSync(`${dataFolder}/${name}/manager.json`, JSON.stringify(payload));
+    // } else {
+    //   const num = Number(fileList[fileList.length - 1].split(MANAGER_FILE_PREFIX)[1])
+    //   fs.writeFileSync(`${dataFolder}/${MANAGER_FILE_PREFIX}${num + 1}`, JSON.stringify(payload))
+    // }
 };
 exports.createManager = createManager;
 //# sourceMappingURL=manager.js.map
--- src/main/services/manager.ts
+++ src/main/services/manager.ts
@@ -1,20 +1,24 @@
 import fs from 'fs'
 import path from 'path'
+import isDev from 'electron-is-dev'
 import { MANAGER_FILE_PREFIX, ManagerWithConfig } from '../types/models/Manager'
+import { v4 } from 'uuid'
 
 export const createManager = (payload: ManagerWithConfig) => {
-  const dataFolder =  path.join(__dirname, '..', 'data')
+  const dataFolder = isDev ? path.join(__dirname, '..', 'data') : path.join(process.resourcesPath, 'data')
   /* Data data folder is not exist */
   if (!fs.existsSync(dataFolder)) {
     fs.mkdirSync(dataFolder)
   }
 
-  const fileList = fs.readdirSync(dataFolder).filter(file => file.includes(MANAGER_FILE_PREFIX))
+  // const fileList = fs.readdirSync(dataFolder).filter(file => file.includes(MANAGER_FILE_PREFIX))
   /* First slot */
-  if (!fileList.length) {
-    fs.writeFileSync(`${dataFolder}/${MANAGER_FILE_PREFIX}1`, JSON.stringify(payload))
-  } else {
-    const num = Number(fileList[fileList.length - 1].split(MANAGER_FILE_PREFIX)[1])
-    fs.writeFileSync(`${dataFolder}/${MANAGER_FILE_PREFIX}${num + 1}`, JSON.stringify(payload))
-  }
+  // if (!fileList.length) {
+  const name = v4()
+  fs.mkdirSync(`${dataFolder}/${name}`)
+  fs.writeFileSync(`${dataFolder}/${name}/manager.json`, JSON.stringify(payload))
+  // } else {
+  //   const num = Number(fileList[fileList.length - 1].split(MANAGER_FILE_PREFIX)[1])
+  //   fs.writeFileSync(`${dataFolder}/${MANAGER_FILE_PREFIX}${num + 1}`, JSON.stringify(payload))
+  // }
 }
