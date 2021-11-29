1fc50f7799023c15d0333810455d89fde7da08b6
kkan0615 <kkan0615@gmail.com>
2021-11-29 11:53:40 +0900

    :rocket: verson 0.0.6

---
+++ src/renderer/views/generals/schedules/Base/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    BaseSchedule
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'BaseSchedule',
+}
+</script>
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
---
+++ src/renderer/views/generals/schedules/Form/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    FormSchedule
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'FormSchedule',
+}
+</script>
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
